4e722731111bd01a3f3c1873d8f8017c05bef2bd
John Pinkerton <jpinkerton88@gmail.com>
2015-06-29 23:50:35 +0000

    remove sidebar

--- wp-content/themes/ggc-clemson/page.php
+++ wp-content/themes/ggc-clemson/page.php
@@ -38,6 +38,4 @@
 		<!-- /section -->
 	</main>
 
-<?php get_sidebar(); ?>
-
 <?php get_footer(); ?>
--- wp-content/themes/ggc-clemson/single.php
+++ wp-content/themes/ggc-clemson/single.php
@@ -60,6 +60,4 @@
 	<!-- /section -->
 	</main>
 
-<?php get_sidebar(); ?>
-
 <?php get_footer(); ?>
