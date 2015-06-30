02432fd93fd07aecfa377c66bd26fc73144bf4eb
John Pinkerton <jpinkerton88@gmail.com>
2015-06-30 01:28:08 +0000

    add home page first block

--- wp-content/themes/ggc-clemson/home-page.php
+++ wp-content/themes/ggc-clemson/home-page.php
@@ -12,13 +12,26 @@
       <!-- article -->
       <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
         
-        <div class="container">
-          <div class="row">
-            <div class="col-md-4 col-md-offset-2">
-              <img src="<?php echo get_template_directory_uri(); ?>/img/clemson-logo.png" />
+        <div class="block-half">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-4 col-md-offset-2">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/clemson-logo.png" />
+              </div>
+              <div class="col-md-4">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/ggc-logo.png" />
+              </div>
             </div>
-            <div class="col-md-4">
-              <img src="<?php echo get_template_directory_uri(); ?>/img/ggc-logo.png" />
+          </div>
+        </div>
+        
+        <div class="block text-block">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-7 col-centered">
+                <h3><?php the_field('home_block_1_title'); ?></h3>
+                <?php the_field('home_block_1_text'); ?>
+              </div>
             </div>
           </div>
         </div>
