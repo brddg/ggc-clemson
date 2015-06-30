f79405da54b9f75363258f1de63ec4a11c73b798
John Pinkerton <jpinkerton88@gmail.com>
2015-06-30 01:13:37 +0000

    add home page logos

--- wp-content/themes/ggc-clemson/home-page.php
+++ wp-content/themes/ggc-clemson/home-page.php
@@ -12,6 +12,16 @@
       <!-- article -->
       <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
         
+        <div class="container">
+          <div class="row">
+            <div class="col-md-4 col-md-offset-2">
+              <img src="<?php echo get_template_directory_uri(); ?>/img/clemson-logo.png" />
+            </div>
+            <div class="col-md-4">
+              <img src="<?php echo get_template_directory_uri(); ?>/img/ggc-logo.png" />
+            </div>
+          </div>
+        </div>
         
 
       </article>
