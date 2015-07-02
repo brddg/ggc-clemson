93f76921a66ca6d05242c6ca2fde9e35235096bc
John Pinkerton <jpinkerton88@gmail.com>
2015-07-02 14:24:45 +0000

    header and hero fixes for mobile

--- wp-content/themes/ggc-clemson/header.php
+++ wp-content/themes/ggc-clemson/header.php
@@ -27,8 +27,21 @@
 			<header class="header clear" role="banner">
 
 					<!-- nav -->
-					<nav class="nav" role="navigation">
-						<?php html5blank_nav(); ?>
+					<nav class="nav navbar navbar-default navbar-fixed-top" role="navigation">
+            <div class="container-fluid">
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                </button>
+              </div>
+              
+              <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <?php html5blank_nav(); ?>
+              </div>
+            </div>
 					</nav>
 					<!-- /nav -->
 
