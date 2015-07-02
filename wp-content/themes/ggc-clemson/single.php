bade14972a7a51afa0440d87417d042cd48500f4
John Pinkerton <jpinkerton88@gmail.com>
2015-07-02 22:23:34 +0000

    add blog single

--- wp-content/themes/ggc-clemson/single.php
+++ wp-content/themes/ggc-clemson/single.php
@@ -1,3 +1,8 @@
+<?php
+  $previousPostLink = get_permalink(get_adjacent_post(false,'',false));
+  $nextPostLink = get_permalink(get_adjacent_post(false,'',true));
+?>
+
 <?php get_header(); ?>
 
 	<main role="main">
@@ -9,35 +14,36 @@
 		<!-- article -->
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-			<!-- post thumbnail -->
-			<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-					<?php the_post_thumbnail(); // Fullsize image for the single post ?>
-				</a>
-			<?php endif; ?>
-			<!-- /post thumbnail -->
-
 			<!-- post title -->
-			<h1>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-			</h1>
+      <div class="container title-block" style="margin-bottom:0;">
+        <div class="row">
+          <div class="col-md-8 col-md-offset-2">
+            <h1><?php the_title(); ?></h1>
+          </div>
+        </div>
+      </div>
 			<!-- /post title -->
 
 			<!-- post details -->
-			<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
-			<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-			<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
+			<p class="small text-center"><?php the_time('F j, Y'); ?></p>
 			<!-- /post details -->
 
-			<?php the_content(); // Dynamic Content ?>
-
-			<?php the_tags( __( 'Tags: ', 'html5blank' ), ', ', '<br>'); // Separated by commas with a line break at the end ?>
-
-			<p><?php _e( 'Categorised in: ', 'html5blank' ); the_category(', '); // Separated by commas ?></p>
-
-			<p><?php _e( 'This post was written by ', 'html5blank' ); the_author(); ?></p>
-
-			<?php edit_post_link(); // Always handy to have Edit Post Links available ?>
+      <div class="container">
+        <div class="col-md-8 col-md-offset-2">
+          <?php the_content(); // Dynamic Content ?>
+        </div>
+      </div>
+      
+      <div class="container">
+        <div class="col-md-8 col-md-offset-2 text-center">
+          <?php if ($previousPostLink !== get_permalink()): ?>
+            <a href="<?php echo $previousPostLink; ?>" class="btn-white black">Previous Article</a>
+          <?php endif; ?>
+          <?php if ($nextPostLink !== get_permalink()): ?>
+            <a href="<?php echo $nextPostLink; ?>" class="btn-white black">Next Article</a>
+          <?php endif; ?>
+        </div>
+      </div>
 
 		</article>
 		<!-- /article -->
