889950dd2ca4aad49e12065ef465a5c72044cabd
John Pinkerton <jpinkerton88@gmail.com>
2015-07-02 21:55:44 +0000

    add blog index

--- wp-content/themes/ggc-clemson/index.php
+++ wp-content/themes/ggc-clemson/index.php
@@ -3,17 +3,23 @@
 	<main role="main">
 		<!-- section -->
 		<section>
+      
+      <div class="container title-block">
+        <div class="row">
+          <div class="col-md-8 col-md-offset-2">
+            <h1><?php _e( 'Blog', 'html5blank' ); ?></h1>
+          </div>
+        </div>
+      </div>
 
-			<h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1>
-
-			<?php get_template_part('loop'); ?>
+      <div class="container the-loop">
+        <?php get_template_part('loop'); ?>
+      </div>
 
 			<?php get_template_part('pagination'); ?>
 
 		</section>
 		<!-- /section -->
 	</main>
-
-<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
--- wp-content/themes/ggc-clemson/loop.php
+++ wp-content/themes/ggc-clemson/loop.php
@@ -3,29 +3,29 @@
 	<!-- article -->
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-		<!-- post thumbnail -->
-		<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
-			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-				<?php the_post_thumbnail(array(120,120)); // Declare pixel size you need inside the array ?>
-			</a>
-		<?php endif; ?>
-		<!-- /post thumbnail -->
-
 		<!-- post title -->
-		<h2>
-			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-		</h2>
+    <div class="row">
+      <div class="col-md-7 col-centered">
+        <h2 class="text-center">
+          <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+        </h2>
+      </div>
+    </div>
 		<!-- /post title -->
 
-		<!-- post details -->
-		<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
-		<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-		<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
-		<!-- /post details -->
+    <div class="row">
+      <div class="col-md-8 col-md-offset-2">
+        <!-- post details -->
+        <p class="small text-center text-black"><?php the_time('F j, Y'); ?></p>
+        <!-- /post details -->
 
-		<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
+        <div class="text-center loop-excerpt"><?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?></div>
 
-		<?php edit_post_link(); ?>
+        <p class="small text-center"><a href="<?php the_permalink(); ?>">Keep Reading</a></p>
+        
+        <div class="border-me"></div>
+      </div>
+    </div>
 
 	</article>
 	<!-- /article -->
